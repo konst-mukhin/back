9ba4f489439b31c352f1ffdcc4a202cc08c99e21
konst-mukhin <konstantin.mukhinn@gmail.com>
2025-03-21 02:02:27 +0300

    add authentication

--- jcrpo_authentication/src/main/java/com/example/demo/DemoApplication.java
+++ jcrpo_authentication/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package org.example.jcrpo_authentication;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JcrpoAuthenticationApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JcrpoAuthenticationApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 
 }
